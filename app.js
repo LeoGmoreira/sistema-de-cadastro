abebbfe0314f59a0bdef322e5bb2b86b5802c0e7
Leonardo Moreira <leonardo@mastersig.com.br>
2022-10-10 20:04:35 -0300

    Voltando atras

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("A aplicacao está conectando com o banco de dados")
-
-//Mais um log de teste
-console.log("O paulo fez esse commit sem ver na aula")
-console.log("Enviando para o git hub");
