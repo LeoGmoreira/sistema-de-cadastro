fab7093859f0c094f53394e6642a742959606c80
Leonardo Moreira <leonardo@mastersig.com.br>
2022-10-10 19:49:54 -0300

    Paulo fez

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicacao está conectando com o banco de dados")
+
+//Mais um log de teste
+console.log("O paulo fez esse commit sem ver na aula")
